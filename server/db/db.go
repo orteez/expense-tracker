c8d442ebcb667ddb112728fe854fc426320db5ba
Christian Ortiz <chanorti@ucsc.edu>
2020-05-07 19:40:57 -0700

    Updated queries to postgres standard

--- server/db/db.go
+++ server/db/db.go
@@ -56,15 +56,22 @@ func Expenses() []models.Expense {
 
 // AddExpense add expense into database
 func AddExpense(expense models.Expense) {
-	query := fmt.Sprintf("INSERT INTO expenses (text, amount) values ('%s', %d)", expense.Text, expense.Amount)
-	res, err := db.Exec(context.Background(), query)
+	res, err := db.Exec(context.Background(), "INSERT INTO expenses (text, amount) VALUES($1, $2)", expense.Text, expense.Amount)
 	log.Println(res)
 	logFatal(err)
 }
 
 // AddExpense add expense into database
 func DeleteExpense(id int) {
-	query := fmt.Sprintf("DELETE FROM expenses WHERE id = %d", id)
-	res, err := db.Exec(context.Background(), query)
+	res, err := db.Exec(context.Background(), "DELETE FROM expenses WHERE id = $1", id)
 	logFatal(err)
+	log.Println(res)
+}
+
+// AddExpense add expense into database
+func EditExpense(expense models.Expense) {
+	res, err := db.Exec(context.Background(), "UPDATE expense SET amount=$1, text=$2 WHERE id=$3", expense.Amount, expense.Text, expense.ID)
+	logFatal(err)
+	log.Println(res)
+
 }
